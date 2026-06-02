c971548b26e9a5e276feb5fb2c0d4a9326c280d5
StefanSerba <stefanserba19@gmail.com>
2026-06-02 21:27:20 +0300

    Add files via upload

---
+++ js/shared.js
@@ -0,0 +1,4 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+hamburger.addEventListener('click', () => mobileMenu.classList.toggle('open'));
+function closeMobile() { mobileMenu.classList.remove('open'); }
