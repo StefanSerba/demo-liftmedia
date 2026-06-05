c901273d66615dc42f82eccf930eb4aced43c0a1
StefanSerba <stefanserba19@gmail.com>
2026-06-05 11:23:21 +0300

    Delete js directory

--- js/shared.js
+++
@@ -1,4 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
-hamburger.addEventListener('click', () => mobileMenu.classList.toggle('open'));
-function closeMobile() { mobileMenu.classList.remove('open'); }
